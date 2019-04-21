3f39650e790e61af1c1bdca277389b4d2bb59e94
caoyu <1012217944@qq.com>
2019-04-21 17:02:05 +0800

    角色管理

---
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/RoleMapperExtra.java
@@ -0,0 +1,17 @@
+package com.cqut.czb.bn.dao.mapper;
+
+import com.cqut.czb.bn.entity.dto.role.RoleInputDTO;
+import com.cqut.czb.bn.entity.entity.Role;
+
+import java.util.List;
+
+public interface RoleMapperExtra {
+
+    int insertRole(RoleInputDTO roleInputDTO);
+
+    int deleteRole(RoleInputDTO roleInputDTO);
+
+    int updateRole(RoleInputDTO roleInputDTO);
+
+    List<Role> selectRole(RoleInputDTO roleInputDTO);
+}
