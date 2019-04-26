ec34f68299e86b104d40bbe23924dc2e30fe5191
LinQiang <1766916065@qq.com>
2019-04-26 23:39:23 +0800

    地址管理

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/userAddress/UserAddressServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/userAddress/UserAddressServiceImpl.java
@@ -1,19 +1,48 @@
 package com.cqut.czb.bn.service.impl.userAddress;
 
+import com.cqut.czb.bn.dao.mapper.AddressMapper;
 import com.cqut.czb.bn.dao.mapper.AddressMapperExtra;
 import com.cqut.czb.bn.entity.entity.Address;
 import com.cqut.czb.bn.service.userAddress.IUserAddressService;
+import com.cqut.czb.bn.util.string.StringUtil;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 @Service
 public class UserAddressServiceImpl implements IUserAddressService {
     @Autowired
     AddressMapperExtra addressMapperExtra;
+
+    @Autowired
+    AddressMapper addressMapper;
     @Override
     public List<Address> getUserAddressList(String userId) {
         return addressMapperExtra.getUserAddressList(userId);
+    }
+
+    @Override
+    public boolean addAddress(Address address, String userId) {
+        address.setUserId(userId);
+        address.setAddressId(StringUtil.createId());
+        address.setCreateAt(new Date());
+        return addressMapper.insert(address)>0;
+    }
+
+    @Override
+    public boolean modifyAddress(Address address){
+        address.setUpdateAt(new Date());
+        return  addressMapper.updateByPrimaryKeySelective(address) > 0;
+    }
+
+    @Override
+    public boolean deleteAddress(Address address, String userId) {
+        if(addressMapperExtra.getUserAddressList(userId).size() <= 1){
+            return false;
+        }
+
+        return addressMapper.deleteByPrimaryKey(address.getAddressId()) > 0;
     }
 }
