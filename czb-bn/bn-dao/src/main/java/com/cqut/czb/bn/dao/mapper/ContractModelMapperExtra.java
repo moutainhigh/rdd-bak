e90bf2e908f5a392c83d8febcfcc2906536ce142
caoyu <1012217944@qq.com>
2019-04-30 21:10:50 +0800

    extra

---
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/ContractModelMapperExtra.java
@@ -0,0 +1,8 @@
+package com.cqut.czb.bn.dao.mapper;
+
+import com.cqut.czb.bn.entity.entity.ContractModel;
+
+public interface ContractModelMapperExtra {
+
+    int insertContractModel(ContractModel record);
+}
