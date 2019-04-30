d9d74b664a9a872adc52fcaafdddfd2addd02b81
Lostleft <1752036320@qq.com>
2019-04-30 11:19:34 +0800

    实体修改

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/Announcement.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/Announcement.java
@@ -1,5 +1,9 @@
 package com.cqut.czb.bn.entity.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.Date;
 
 public class Announcement {
@@ -7,7 +11,7 @@ public class Announcement {
 
     private String announcementTitle;
 
-    private String announcementContent;
+    private String announcementContent; //公告内容
 
     private Integer announcementType;
 
@@ -15,13 +19,17 @@ public class Announcement {
 
     private Integer isShow;
 
-    private Integer announcementOrder;
+    private Integer announcementOrder; //顺序
 
     private String locationCode;
-
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date createAt;
-
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date updateAt;
+
+    private Integer pageNum;
+
+    private Integer pageSize;
 
     public String getAnnouncementId() {
         return announcementId;
@@ -101,5 +109,21 @@ public class Announcement {
 
     public void setUpdateAt(Date updateAt) {
         this.updateAt = updateAt;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
     }
 }
