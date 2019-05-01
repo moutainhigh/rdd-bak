afc36518ed8876fb44828e5a0042f257d0ca724e
yule15826045613 <774933808@qq.com>
2019-05-01 21:51:55 +0800

    一、模块：租车服务 1. 新增接口 (1). 个人租车服务合同列表获取 (2). 个人合同概要获取 (3). 查看个人收益(待修改)

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/rentCarServer/RentCarController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/rentCarServer/RentCarController.java
@@ -1,21 +1,46 @@
 package com.cqut.czb.bn.api.controller.rentCarServer;
 
+import com.cqut.czb.bn.entity.dto.rentCar.OneContractInfoInputDTO;
 import com.cqut.czb.bn.entity.global.JSONResult;
 import com.cqut.czb.bn.service.rentCarService.RentCarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/renCar")
-@CrossOrigin
+@RestController
 public class RentCarController {
     @Autowired
     private RentCarService rentCarService;
 
+    /**
+     * 获取个人收益记录
+     * @return JSONResult
+     */
     @RequestMapping(value = "/getPersonIncome", method = RequestMethod.GET)
     public JSONResult getPersonIncome(){
         return new JSONResult(rentCarService.getPersonIncome());
+    }
+
+    /**
+     * 获取合同概要信息
+     * @param oneContractInfoInputDTO
+     * @return JSONResult
+     */
+    @RequestMapping(value = "/getOneContractInfo", method = RequestMethod.POST)
+    public JSONResult getOneContractInfo(@Validated OneContractInfoInputDTO oneContractInfoInputDTO){
+        return new JSONResult(rentCarService.getOneContractInfo(oneContractInfoInputDTO.getContractId()));
+    }
+
+    /**
+     * 个人租车服务，合同列表获取
+     */
+    @RequestMapping(value = "/getPersonalContractList", method = RequestMethod.GET)
+    public JSONResult getPersonalContractList(){
+        return new JSONResult(rentCarService.getPersonalContractList());
     }
 
 }
--- czb-bn/bn-service/src/main/java/com/cqut/czb/bn/service/rentCarService/RentCarService.java
+++ czb-bn/bn-service/src/main/java/com/cqut/czb/bn/service/rentCarService/RentCarService.java
@@ -1,7 +1,17 @@
 package com.cqut.czb.bn.service.rentCarService;
 
+import com.cqut.czb.bn.entity.dto.rentCar.OneContractInfoDTO;
+import com.cqut.czb.bn.entity.dto.rentCar.OneContractInfoInputDTO;
+import com.cqut.czb.bn.entity.dto.rentCar.PersonalContractDTO;
 import com.cqut.czb.bn.entity.dto.rentCar.personIncome;
+
+import java.util.List;
 
 public interface RentCarService {
     personIncome getPersonIncome();
+
+    List<OneContractInfoDTO> getOneContractInfo(String contractId);
+
+    List<PersonalContractDTO> getPersonalContractList();
+
 }
