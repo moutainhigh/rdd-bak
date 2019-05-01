08bdb926fedcba6c9c76470132e782b4c0c92081
Lostleft <1752036320@qq.com>
2019-05-01 22:46:47 +0800

    1.时间查询修改 2.油卡寄送功能

---
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/PetrolDeliveryRecordsController.java
@@ -0,0 +1,15 @@
+package com.cqut.czb.bn.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**   PetrolDeliveryRecordsController  油卡快递模块
+ *
+ */
+@RequestMapping("/petrolDelivery")
+@RestController
+public class PetrolDeliveryRecordsController {
+
+
+
+}
---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/petrolDeliveryRecords/PetrolDeliveryDTO.java
@@ -0,0 +1,145 @@
+package com.cqut.czb.bn.entity.dto.petrolDeliveryRecords;
+
+import java.util.Date;
+
+public class PetrolDeliveryDTO {
+    private String recordId;
+
+    private String petrolNum;
+
+    private Integer petrolKind;
+
+    private String addressId;
+
+    private Integer deliveryState;
+
+    private String deliveryNum;
+
+    private String deliveryCompany;
+
+    private String receiver;
+
+    private String contactNumber;
+
+    private String province;
+
+    private String city;
+
+    private String detail;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId == null ? null : recordId.trim();
+    }
+
+    public String getPetrolNum() {
+        return petrolNum;
+    }
+
+    public void setPetrolNum(String petrolNum) {
+        this.petrolNum = petrolNum == null ? null : petrolNum.trim();
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId == null ? null : addressId.trim();
+    }
+
+    public Integer getDeliveryState() {
+        return deliveryState;
+    }
+
+    public void setDeliveryState(Integer deliveryState) {
+        this.deliveryState = deliveryState;
+    }
+
+    public String getDeliveryNum() {
+        return deliveryNum;
+    }
+
+    public void setDeliveryNum(String deliveryNum) {
+        this.deliveryNum = deliveryNum == null ? null : deliveryNum.trim();
+    }
+
+    public String getDeliveryCompany() {
+        return deliveryCompany;
+    }
+
+    public void setDeliveryCompany(String deliveryCompany) {
+        this.deliveryCompany = deliveryCompany == null ? null : deliveryCompany.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Integer getPetrolKind() {
+        return petrolKind;
+    }
+
+    public void setPetrolKind(Integer petrolKind) {
+        this.petrolKind = petrolKind;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
