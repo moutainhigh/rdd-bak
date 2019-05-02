b09e3bb37ae79ece3dfd7175487142c6f90c8b21
caoyu <1012217944@qq.com>
2019-05-02 09:54:08 +0800

    1

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/MenuServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/MenuServiceImpl.java
@@ -13,10 +13,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class MenuServiceImpl implements IMenuService {
 
     @Autowired
--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/UserServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/UserServiceImpl.java
@@ -18,11 +18,13 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements IUserService {
 
     @Autowired
