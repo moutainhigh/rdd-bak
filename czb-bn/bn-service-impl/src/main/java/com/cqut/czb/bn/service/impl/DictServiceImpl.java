13516fc96358f5a006862b992c9790a8d8bdc557
caoyu <1012217944@qq.com>
2019-05-12 01:41:14 +0800

    字典----客服获取

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/DictServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/DictServiceImpl.java
@@ -1,16 +1,22 @@
 package com.cqut.czb.bn.service.impl;
 
+import com.cqut.czb.bn.dao.mapper.DictMapperExtra;
 import com.cqut.czb.bn.entity.entity.Dict;
 import com.cqut.czb.bn.service.IDictService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DictServiceImpl implements IDictService {
+
+    @Autowired
+    DictMapperExtra dictMapperExtra;
+
     @Override
     public List<Dict> selectCustomerServiceStaff() {
 
-        return null;
+        return dictMapperExtra.selectCustomerServiceStaff();
     }
 }
