98725f4330b7ebc11cf13e36b01323c566140734
LinQiang <1766916065@qq.com>
2019-05-13 11:07:39 +0800

    更改支付回调IP为服务器IP

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/paymentRecord/AiHuAlipayConfig.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/paymentRecord/AiHuAlipayConfig.java
@@ -18,10 +18,10 @@ public class AiHuAlipayConfig {
 //	public static String notify_url = "http://222.178.158.125:9080/TinklingCat/api/verifyAsyn/verifyAsynNoticeInfoAiHu";
 //	public static String notify_url = SystemConstants.IPAPI+"/czb/api/verifyAsyn/verifyAsynNoticeInfoAiHu";
 	//购买油卡的回调的url
-	public static String notify_url ="http://106.91.24.166:8899/verifyAsyn/verifyAsynNoticeInfoAiHu";
+	public static String notify_url ="http://39.98.250.45:8899/verifyAsyn/verifyAsynNoticeInfoAiHu";
 
 	//充值的回调的url
-	public static String recharge_url="http://106.91.24.166:8899/personCenter/VIP/purchaseVIP";
+	public static String recharge_url="http://39.98.250.45:8899/personCenter/VIP/purchaseVIP";
 
 	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 //	public static String return_url = "http://" + StringUtil.getIpAddress() +":9080/NewCar/api/paymentRecord/verifyAsynNoticeInfo";
