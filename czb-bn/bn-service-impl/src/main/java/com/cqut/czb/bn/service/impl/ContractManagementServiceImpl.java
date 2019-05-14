4c22a3aea3b8d2a5287dad47ffdc0afe43b02817
caoyu <1012217944@qq.com>
2019-05-14 10:23:12 +0800

    删除无用依赖

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/ContractManagementServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/ContractManagementServiceImpl.java
@@ -20,9 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ContractManagementServiceImpl implements IContractService {
