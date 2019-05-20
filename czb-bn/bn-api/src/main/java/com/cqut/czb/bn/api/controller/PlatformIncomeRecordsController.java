aa206b8618a53851ccab42039b40409a59d1e810
Lostleft <1752036320@qq.com>
2019-05-20 15:43:20 +0800

    收付款最初结构

---
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/PlatformIncomeRecordsController.java
@@ -0,0 +1,17 @@
+package com.cqut.czb.bn.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 收款记录
+ */
+@RestController
+@RequestMapping("/api/receive")
+public class PlatformIncomeRecordsController {
+    /**
+     * 获取列表/查询
+     */
+
+}
