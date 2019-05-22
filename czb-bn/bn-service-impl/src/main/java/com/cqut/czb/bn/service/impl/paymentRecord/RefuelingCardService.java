bd5f8ad691be9682360fcf1c038e077ccd6a6c80
Administrator <2784717534>
2019-05-23 02:16:09 +0800

    购买油卡

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/paymentRecord/RefuelingCardService.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/paymentRecord/RefuelingCardService.java
@@ -258,7 +258,7 @@ public class RefuelingCardService implements IRefuelingCard {
         petrolSalesRecords.setPetrolKind(petrol.getPetrolKind());
         petrolSalesRecords.setThirdOrderId(orgId);
         petrolSalesRecords.setRecordType(1);
-        petrolSalesRecords.setIsRecharged(1);
+        petrolSalesRecords.setIsRecharged(0);
         boolean insertPetrolSalesRecords = insertPetrolSalesRecords(petrolSalesRecords);
         System.out.println("新增购买记录表完毕" + insertPetrolSalesRecords);
 
