af77f54f109e4e844d90e9434c0cf2a03b7643aa
caoyu <1012217944@qq.com>
2019-05-24 19:23:29 +0800

    登陆逻辑变更

--- czb-auth/src/main/java/com/cqut/czb/auth/config/SecurityConfig.java
+++ czb-auth/src/main/java/com/cqut/czb/auth/config/SecurityConfig.java
@@ -2,12 +2,12 @@ package com.cqut.czb.auth.config;
 
 import com.cqut.czb.auth.filter.JWTAuthenticationFilter;
 import com.cqut.czb.auth.filter.JWTAuthorizationFilter;
+import com.cqut.czb.auth.provider.DaoAuthenticationProvider;
 import jodd.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
