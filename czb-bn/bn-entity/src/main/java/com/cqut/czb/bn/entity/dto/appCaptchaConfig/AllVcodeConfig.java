f810b400932aca07a49f5642a99f81ce6af37e72
Administrator <2784717534>
2019-05-25 19:41:55 +0800

    提交发送短信

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/appCaptchaConfig/AllVcodeConfig.java
+++
@@ -1,5 +0,0 @@
-package com.cqut.czb.bn.entity.dto.appCaptchaConfig;
-
-public class AllVcodeConfig {
-
-}
--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/appCaptchaConfig/VcodeConfig.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/appCaptchaConfig/VcodeConfig.java
@@ -13,11 +13,11 @@ public class VcodeConfig {
     final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
     final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
     // 替换成你的AK (产品密)
-    final static String accessKeyId = "LTAITsFvKsI7oqh8";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
-    final static String accessKeySecret = "Btu2lW2ArROCiRybZ6CV3IqBuFlYuh";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
+    final static String accessKeyId = "LTAIBast8tgc6pfu";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
+    final static String accessKeySecret = "cAwmmIGtUAmfrY9bbpAWV197NuW9fj";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
     // 必填:短信签名-可在短信控制台中找到
-    final static String SignName = "租车宝";// 阿里云配置你自己的短信签名填入
+    final static String SignName = "人多多认证";// 阿里云配置你自己的短信签名填入
     // 必填:短信模板-可在短信控制台中找到
-    final static String TemplateCode = "SMS_166080476"; // 阿里云配置你自己的短信模板填入
+    final static String TemplateCode = "SMS_166371667"; // 阿里云配置你自己的短信模板填入
 
 }
