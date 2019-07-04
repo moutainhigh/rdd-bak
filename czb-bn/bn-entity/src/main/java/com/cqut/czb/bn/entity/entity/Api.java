86e209fbefbab33208311a8c17c464d35b178fbf
caoyu <1012217944@qq.com>
2019-07-04 21:05:15 +0800

    数据库新增

---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/Api.java
@@ -0,0 +1,75 @@
+package com.cqut.czb.bn.entity.entity;
+
+import java.util.Date;
+
+public class Api {
+    private String apiId;
+
+    private String apiUrl;
+
+    private String apiName;
+
+    private String apiRemark;
+
+    private String pid;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId == null ? null : apiId.trim();
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public void setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl == null ? null : apiUrl.trim();
+    }
+
+    public String getApiName() {
+        return apiName;
+    }
+
+    public void setApiName(String apiName) {
+        this.apiName = apiName == null ? null : apiName.trim();
+    }
+
+    public String getApiRemark() {
+        return apiRemark;
+    }
+
+    public void setApiRemark(String apiRemark) {
+        this.apiRemark = apiRemark == null ? null : apiRemark.trim();
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid == null ? null : pid.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/RoleApi.java
@@ -0,0 +1,55 @@
+package com.cqut.czb.bn.entity.entity;
+
+import java.util.Date;
+
+public class RoleApi {
+    private String id;
+
+    private String roleId;
+
+    private String apiId;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId == null ? null : apiId.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
