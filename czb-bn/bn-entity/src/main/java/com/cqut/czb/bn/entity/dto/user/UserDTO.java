f91a33c8752929d10348ab1ec46bb779905f4b4c
caoyu <1012217944@qq.com>
2019-07-05 21:54:32 +0800

    更改redis获取数据

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/user/UserDTO.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/user/UserDTO.java
@@ -2,9 +2,7 @@ package com.cqut.czb.bn.entity.dto.user;
 
 import com.cqut.czb.bn.entity.dto.role.RoleDTO;
 import com.cqut.czb.bn.entity.entity.User;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
-import java.util.Date;
 import java.util.List;
 
 public class UserDTO extends User {
@@ -12,17 +10,6 @@ public class UserDTO extends User {
     private List<RoleDTO> roleList;
 
     private String roleId;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date createAt;
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
 
     public List<RoleDTO> getRoleList() {
         return roleList;
