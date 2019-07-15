f37bf0c4f9539612199df20ce0ad07f5f630ee83
yule15826045613 <774933808@qq.com>
2019-07-15 11:31:33 +0800

    新建补贴任务表、用户id关系表

---
+++ czb-global/src/main/resources/db/migration/V1.8__add_subsidy_user_relation.sql
@@ -0,0 +1,14 @@
+-- ----------------------------
+-- Table structure for czb_subsidy_user
+-- ----------------------------
+DROP TABLE IF EXISTS `czb_subsidy_user`;
+CREATE TABLE `czb_subsidy_user` (
+  `relation_id` varchar(20) NOT NULL,
+  `mission_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`relation_id`),
+  KEY `user_id` (`user_id`),
+  KEY `mission_id` (`mission_id`),
+  CONSTRAINT `czb_subsidy_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `czb_user` (`user_id`),
+  CONSTRAINT `czb_subsidy_user_ibfk_2` FOREIGN KEY (`mission_id`) REFERENCES `czb_subsidy_mission` (`mission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
