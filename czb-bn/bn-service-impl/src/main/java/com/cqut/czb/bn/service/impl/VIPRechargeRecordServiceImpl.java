bccfe111f79422d3b06774d655b3a0881d6af39f
xuhaodong1 <1347929462@qq.com>
2019-07-18 22:37:08 +0800

    IMpl

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/VIPRechargeRecordServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/VIPRechargeRecordServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cqut.czb.bn.service.impl;
 
+import com.cqut.czb.bn.service.VIPRechargeRecordService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Service;
  * @create 2019-07-18 22:25
  */
 @Service
-public class VIPRechargeRecordServiceImpl {
+public class VIPRechargeRecordServiceImpl implements VIPRechargeRecordService {
 
 }
