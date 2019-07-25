e01646e85f66f822586cc769a824c7530e1a1182
Administrator <2784717534>
2019-07-25 16:47:12 +0800

    油卡展示

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/AppHomePageController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/AppHomePageController.java
@@ -1,6 +1,7 @@
 package com.cqut.czb.bn.api.controller;
 
 import com.cqut.czb.auth.util.RedisUtils;
+import com.cqut.czb.bn.dao.mapper.UserMapper;
 import com.cqut.czb.bn.entity.dto.appPersonalCenter.AppRouterDTO;
 import com.cqut.czb.bn.entity.entity.User;
 import com.cqut.czb.bn.entity.global.JSONResult;
@@ -32,6 +33,10 @@ public class AppHomePageController {
 
     @Autowired
     RedisUtils redisUtils;
+
+    @Autowired
+    UserMapper userMapper;
+
     /**
      * app广告展示
      * @return
@@ -67,17 +72,19 @@ public class AppHomePageController {
      * @return
      */
     @RequestMapping(value = "/selectPetrolZone",method = RequestMethod.GET)
-    public JSONResult selectPetrol(Principal principal, @RequestParam(name="area") String area){
+    public JSONResult selectPetrol(Principal principal, @RequestParam(name="area") String area) {
 
-        User user = (User)redisUtils.get(principal.getName());
+        User user = (User) redisUtils.get(principal.getName());
 
         if (user.getUserAccount().equals("15870596710") || user.getUserAccount().equals("15520024205")) {
-            area="重庆市";
+            area = "重庆市";
         }
-        if(user.getVersionNum().equals("1.0.3")){
+        //暂留
+        User user1 = userMapper.selectByPrimaryKey(user.getUserId());
+
+        if (user1==null||user1.getVersionNum().equals("1.0.3")) {
             return new JSONResult("暂无油卡", ResponseCodeConstants.FAILURE);
         }
-
         if(area==null)
             return new JSONResult("无法获取当前位置", ResponseCodeConstants.FAILURE);
         return new JSONResult(appHomePageService.selectPetrolZone(area));
