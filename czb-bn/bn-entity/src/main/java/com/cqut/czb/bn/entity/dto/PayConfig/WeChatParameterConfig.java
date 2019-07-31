e5eb0656c2f83e2842edd181750f125981e0ce50
xuhaodong <1347929462@qq.com>
2019-07-31 14:40:41 +0800

    消息管理

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/PayConfig/WeChatParameterConfig.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/PayConfig/WeChatParameterConfig.java
@@ -57,7 +57,7 @@ public class WeChatParameterConfig {
             totalFee = BigDecimal.valueOf(petrolInputDTO.getPetrolPrice()).multiply(new BigDecimal(100)).multiply(new BigDecimal(petrol.getDiscount()))
                     .toBigInteger();
         }else{
-            totalFee = BigDecimal.valueOf(petrolInputDTO.getPetrolPrice()).toBigInteger();
+            totalFee = BigDecimal.valueOf(petrolInputDTO.getPetrolPrice()).multiply(new BigDecimal(100).toBigInteger();
         }
 
         System.out.println(totalFee);
