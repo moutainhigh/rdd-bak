35e35492921529498027e5b0b963f4240572802c
yule15826045613 <774933808@qq.com>
2019-08-13 10:48:45 +0800

    评价表实体类

---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/vehicleService/RiderEvaluate.java
@@ -0,0 +1,65 @@
+package com.cqut.czb.bn.entity.entity.vehicleService;
+
+import java.util.Date;
+
+public class RiderEvaluate {
+    private String evaluateId;
+
+    private String evaluateRiderId;
+
+    private Float evaluateLevel;
+
+    private String evaluateMessage;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public String getEvaluateId() {
+        return evaluateId;
+    }
+
+    public void setEvaluateId(String evaluateId) {
+        this.evaluateId = evaluateId == null ? null : evaluateId.trim();
+    }
+
+    public String getEvaluateRiderId() {
+        return evaluateRiderId;
+    }
+
+    public void setEvaluateRiderId(String evaluateRiderId) {
+        this.evaluateRiderId = evaluateRiderId == null ? null : evaluateRiderId.trim();
+    }
+
+    public Float getEvaluateLevel() {
+        return evaluateLevel;
+    }
+
+    public void setEvaluateLevel(Float evaluateLevel) {
+        this.evaluateLevel = evaluateLevel;
+    }
+
+    public String getEvaluateMessage() {
+        return evaluateMessage;
+    }
+
+    public void setEvaluateMessage(String evaluateMessage) {
+        this.evaluateMessage = evaluateMessage == null ? null : evaluateMessage.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
