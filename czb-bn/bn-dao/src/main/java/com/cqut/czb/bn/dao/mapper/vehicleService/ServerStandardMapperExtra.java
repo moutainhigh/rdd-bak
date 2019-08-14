36533ad815a57b85d8e240a1d06aefe5c3190622
Administrator <2784717534>
2019-08-14 20:55:55 +0800

    信息录入

--- czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/vehicleService/ServerStandardMapperExtra.java
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/vehicleService/ServerStandardMapperExtra.java
@@ -1,11 +1,14 @@
 package com.cqut.czb.bn.dao.mapper.vehicleService;
 
 import com.cqut.czb.bn.entity.dto.appCarWash.ServiceCommodityDTO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface ServerStandardMapperExtra {
 
     List<ServiceCommodityDTO> selectServiceInfo();
+
+    ServiceCommodityDTO selectOneService(@Param("serverId") String serverId);
 
 }
---
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/AppPaymentService/AppBuyCarWashServiceImpl.java
@@ -0,0 +1,118 @@
+package com.cqut.czb.bn.service.impl.AppPaymentService;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.request.AlipayTradeAppPayRequest;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
+import com.cqut.czb.bn.dao.mapper.vehicleService.CleanServerVehicleMapperExtra;
+import com.cqut.czb.bn.dao.mapper.vehicleService.VehicleCleanOrderMapperExtra;
+import com.cqut.czb.bn.entity.dto.PayConfig.*;
+import com.cqut.czb.bn.entity.dto.appBuyCarWashService.CleanServerVehicleDTO;
+import com.cqut.czb.bn.entity.entity.User;
+import com.cqut.czb.bn.entity.entity.vehicleService.CleanServerVehicle;
+import com.cqut.czb.bn.entity.entity.vehicleService.VehicleCleanOrder;
+import com.cqut.czb.bn.service.AppBuyCarWashService;
+import com.cqut.czb.bn.util.string.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.SortedMap;
+import java.util.UUID;
+
+@Service("AppBuyCarWashService")
+public class AppBuyCarWashServiceImpl implements AppBuyCarWashService {
+
+    @Autowired
+    VehicleCleanOrderMapperExtra vehicleCleanOrderMapperExtra;
+
+    @Autowired
+    CleanServerVehicleMapperExtra cleanServerVehicleMapperExtra;
+
+    @Override
+    public String AliBuyCarWash(User user, CleanServerVehicleDTO cleanServerVehicleDTO) {
+        //判空
+        if(user==null&&cleanServerVehicleDTO==null){
+            System.out.println("用户信息不全");
+            return null;
+        }
+        //生成起吊参数
+        /**
+         * 生成起调参数串——返回给app（支付宝的支付订单）
+         */
+        String orderString = null;//用于保存起调参数,
+        AlipayClientConfig alipayClientConfig = AlipayClientConfig.getInstance("4");//"3"为购买服务
+        AlipayClient alipayClient = alipayClientConfig.getAlipayClient();
+        AlipayTradeAppPayRequest request = new AlipayTradeAppPayRequest();
+        //订单标识
+        String thirdOrder = System.currentTimeMillis() + UUID.randomUUID().toString().substring(10, 15);
+        //支付金额
+        Double actualPrice=cleanServerVehicleDTO.getServerPrice();
+        //商品id
+        String commodityId=cleanServerVehicleDTO.getService_id();
+        //购买者id
+        String ownerId = user.getUserId();
+        request.setBizModel(AliParameterConfig.getBizModel2(thirdOrder, actualPrice,commodityId ,ownerId));//支付订单
+        request.setNotifyUrl(AliPayConfig.BuyService_url);//支付回调接口
+        try {
+            // 这里和普通的接口调用不同，使用的是sdkExecute
+            AlipayTradeAppPayResponse response = alipayClient.sdkExecute(request);
+            orderString = response.getBody();
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+
+
+        //服务车辆id
+        String vehicleId=StringUtil.createId();
+
+        //插入两表
+        //洗车服务订单记录	czb_vehicle_clean_order 插入预支付订单
+        VehicleCleanOrder vehicleCO=new VehicleCleanOrder();
+        vehicleCO.setServerOrderId(thirdOrder);
+        vehicleCO.setUserId(user.getUserId());
+        //骑手没有放入
+        vehicleCO.setPayStatus((byte)0);
+        //前要注意下
+        vehicleCO.setActualPrice(cleanServerVehicleDTO.getServerPrice());
+        //在下面VehicleId()
+        vehicleCO.setVehicleId(vehicleId);
+        vehicleCO.setProcessStatus((byte)0);
+
+        boolean K=vehicleCleanOrderMapperExtra.insert(vehicleCO)>0;
+        System.out.println("插入洗车服务预支付订单"+K);
+
+        //洗车服务车辆对象表	czb_clean_server_vehicle  录入备份信息
+        CleanServerVehicle cleanSV=new CleanServerVehicle();
+        cleanSV.setVehicleId(vehicleId);
+        cleanSV.setUserId(user.getUserId());
+        cleanSV.setUserName(cleanServerVehicleDTO.getUserName());
+        cleanSV.setLicenseNumber(cleanServerVehicleDTO.getLicenseNumber());
+        cleanSV.setVehicleColor(cleanServerVehicleDTO.getVehicleColor());
+        cleanSV.setVehicleType((byte) cleanServerVehicleDTO.getVehicleType());
+        cleanSV.setVehicleSeries(cleanServerVehicleDTO.getVehicleSeries());
+
+        boolean j=cleanServerVehicleMapperExtra.insert(cleanSV)>0;
+        System.out.println("插入车辆信息"+j);
+
+        return orderString;
+    }
+
+    @Override
+    public JSONObject WeChatBuyCarWash(User user, CleanServerVehicleDTO cleanServerVehicleDTO) {
+        //判空
+        if(user==null&&cleanServerVehicleDTO==null){
+            System.out.println("用户信息不全");
+            return null;
+        }
+
+//        String orgId = System.currentTimeMillis() + UUID.randomUUID().toString().substring(10, 15);
+//        String nonceStrTemp = WeChatUtils.getRandomStr();
+//        // 设置参数
+//        SortedMap<String, Object> parameters = WeChatParameterConfig.getParametersService(nonceStrTemp,orgId,user.getUserId(),commodityDTO);
+//
+//        return  WeChatParameterConfig.getSign( parameters, nonceStrTemp);
+
+        return null;
+    }
+}
