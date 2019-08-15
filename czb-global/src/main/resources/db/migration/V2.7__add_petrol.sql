3915ccfd95e6d859fa95b646470295df94a9c605
Administrator <2784717534>
2019-08-15 10:13:43 +0800

    加字段

--- czb-global/src/main/resources/db/migration/V2.7__add_petrol.sql
+++ czb-global/src/main/resources/db/migration/V2.7__add_petrol.sql
@@ -3,7 +3,7 @@ alter table czb_server_standard add file_id varchar (20) character  set utf8 col
 alter table czb_vehicle_clean_order add server_id varchar (20) character  set utf8 collate utf8_general_ci null default null COMMENT '服务号';
 alter table czb_coupon_standard add is_delete INT( 1 ) DEFAULT 0 COMMENT '0不删除，1是删除';
 alter table czb_rider_evaluate add evaluate_user_id varchar (20) character  set utf8 collate utf8_general_ci null default null COMMENT '评价人id';
-alter table czb_server_standard add service_location varchar (20) character  set utf8 collate utf8_general_ci null default null COMMENT '服务地点';
+alter table czb_clean_server_vehicle add service_location varchar (255) character  set utf8 collate utf8_general_ci null default null COMMENT '服务地点';
 
 
 
