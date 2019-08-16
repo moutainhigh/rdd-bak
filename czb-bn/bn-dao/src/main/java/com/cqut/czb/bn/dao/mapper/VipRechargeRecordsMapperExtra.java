42de9e80077dc0fa39028171e0e75b497d20be2e
e <785342405@qq.com>
2019-08-16 20:48:06 +0800

    commit_lyk_8_16

--- czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/VipRechargeRecordsMapperExtra.java
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/VipRechargeRecordsMapperExtra.java
@@ -3,9 +3,11 @@ package com.cqut.czb.bn.dao.mapper;
 import com.cqut.czb.bn.entity.dto.VIPRechargeRecord.VipRechargeRecordListDTO;
 import com.cqut.czb.bn.entity.entity.VipRechargeRecords;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VipRechargeRecordsMapperExtra {
 
     int insertVipRechargeRecord(VipRechargeRecordListDTO vipRechargeRecordListDTO);
