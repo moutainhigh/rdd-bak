1801f304cde506df1ae211c327288ace171316af
Administrator <2784717534>
2019-08-18 11:58:14 +0800

    优惠劵的获取

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/IssueCoupons/IssueCouponsDTO.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/IssueCoupons/IssueCouponsDTO.java
@@ -1,4 +1,32 @@
 package com.cqut.czb.bn.entity.dto.IssueCoupons;
 
+import java.util.Date;
+
 public class IssueCouponsDTO {
+
+    private String standardId;
+
+    private String standardType;
+
+    private Double standardValue;
+
+    private Integer continueDays;
+
+    private String standardExplain;
+
+    private Integer isDelete;
+
+    private String couponId;
+
+    private Date destroyTime;
+
+    private String couponStandard;
+
+    private String ownerId;
+
+    private int status;
+
+    private Date createAt;
+
+    private Date updateAt;
 }
