ef21b5d2aa241d99ef1ef3b9ba913bccbc1a00db
yule15826045613 <774933808@qq.com>
2019-08-28 09:30:56 +0800

    发送消息接口

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/MessageManagementController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/MessageManagementController.java
@@ -8,10 +8,7 @@ import com.cqut.czb.bn.entity.global.JSONResult;
 import com.cqut.czb.bn.service.MessageManagementService;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -42,7 +39,7 @@ public class MessageManagementController {
 
 
     @PostMapping("/createMsgModel")
-    public JSONResult createMsgModel(MsgModel msgModel, Principal principal){
+    public JSONResult createMsgModel(@RequestBody MsgModel msgModel, Principal principal){
         User user = (User)redisUtils.get(principal.getName());
         msgModel.setAnnouncerId(user.getUserId());
         return new JSONResult(messageManagementService.createMsgModel(msgModel));
