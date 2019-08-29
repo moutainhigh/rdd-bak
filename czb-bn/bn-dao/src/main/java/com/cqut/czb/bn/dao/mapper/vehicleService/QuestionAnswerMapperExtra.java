ff1eb71f841f54b9c86b8476045c84e0a028dcd9
xuhaodong <1347929462@qq.com>
2019-08-29 18:28:48 +0800

    问答

---
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/vehicleService/QuestionAnswerMapperExtra.java
@@ -0,0 +1,14 @@
+package com.cqut.czb.bn.dao.mapper.vehicleService;
+
+import com.cqut.czb.bn.entity.dto.QuestionAnswer.QuestionAnswerDTO;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @auther nihao
+ * @create 2019-08-29 17:06
+ */
+public interface QuestionAnswerMapperExtra {
+    List<QuestionAnswerDTO> selectQuestionAnswerByType(Integer type);
+}
