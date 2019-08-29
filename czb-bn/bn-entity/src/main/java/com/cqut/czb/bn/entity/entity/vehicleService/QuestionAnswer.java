b5aeda7fd97a05d66fc38a9562af6a741e7549c4
Administrator <2784717534>
2019-08-29 15:53:57 +0800

    购买油卡

---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/vehicleService/QuestionAnswer.java
@@ -0,0 +1,65 @@
+package com.cqut.czb.bn.entity.entity.vehicleService;
+
+import java.util.Date;
+
+public class QuestionAnswer {
+    private String questionId;
+
+    private String questionContent;
+
+    private String answerContent;
+
+    private Integer questionType;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId == null ? null : questionId.trim();
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent == null ? null : questionContent.trim();
+    }
+
+    public String getAnswerContent() {
+        return answerContent;
+    }
+
+    public void setAnswerContent(String answerContent) {
+        this.answerContent = answerContent == null ? null : answerContent.trim();
+    }
+
+    public Integer getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(Integer questionType) {
+        this.questionType = questionType;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
