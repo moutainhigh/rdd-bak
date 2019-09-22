c8ead49691f4a91b5e82b4004c9e867f8397448b
Lostleft <1752036320@qq.com>
2019-09-22 20:06:43 +0800

    美食订单表删除commodity_id加入dish_id

--- czb-global/src/main/resources/db/migration/V3.8__update_dish.sql
+++ czb-global/src/main/resources/db/migration/V3.8__update_dish.sql
@@ -1,1 +1,3 @@
+alter table czb_dish_order drop column commodity_id;
+alter table czb_dish_order add dish_id VARCHAR( 20 ) character  set utf8 collate utf8_general_ci null default null COMMENT '菜品ID';
 alter table czb_dish add is_recommend INTEGER( 1 ) DEFAULT 0 COMMENT '是否推荐';
