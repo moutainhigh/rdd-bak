8679319e3ebc0470aa328d5ce538dafbc06834e5
Administrator <2784717534>
2019-09-26 16:42:20 +0800

    点餐

---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/food/AppOrderPage/StartToPay.java
@@ -0,0 +1,11 @@
+package com.cqut.czb.bn.entity.dto.food.AppOrderPage;
+
+import org.apache.poi.ss.formula.functions.T;
+
+public class StartToPay {
+
+    private String orderId;
+
+    private T AliPayOrder;
+
+}
