319fca159eba5b0c888ef631bea5e4722ed65407
yuancaiming <obolozuki@qq.com>
2019-10-19 21:47:35 +0800

    发送短信

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/appCaptchaConfig/PhoneCode.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/appCaptchaConfig/PhoneCode.java
@@ -153,14 +153,12 @@ public class PhoneCode {
         // 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
         request.setPhoneNumbers(userAccount);
         // 必填:短信签名-可在短信控制台中找到
-        request.setSignName(VcodeConfig.SignName);
+        request.setSignName(VcodeConfig.RechargeSignName);
         // 必填:短信模板-可在短信控制台中找到
         request.setTemplateCode(VcodeConfig.RechargeCode);
         // 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-        request.setTemplateParam("{ \"petrolNum\":\""+petrolNum+"\"}");
-        request.setTemplateParam("{ \"userAccount\":\""+userAccount+"\"}");
-        request.setTemplateParam("{ \"petrolDenomination\":\""+petrolDenomination+"\"}");
+        request.setTemplateParam("{ \"petrolNum\":\""+petrolNum+"\", \"userAccount\":\""+userAccount+"\", \"petrolDenomination\":\""+petrolDenomination+"\"}");
         // 可选-上行短信扩展码(无特殊需求用户请忽略此字段)
         // request.setSmsUpExtendCode("90997");
         // 可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
