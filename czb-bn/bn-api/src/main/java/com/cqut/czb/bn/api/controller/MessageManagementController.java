fcbfbdbecd1a8c687d9eddaae6b5246d796369fe
yule15826045613 <774933808@qq.com>
2019-11-01 20:15:31 +0800

    个人消息

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/MessageManagementController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/MessageManagementController.java
@@ -2,7 +2,6 @@ package com.cqut.czb.bn.api.controller;
 
 import com.cqut.czb.auth.util.RedisUtils;
 import com.cqut.czb.bn.entity.dto.MessageManagement.MessageListDTO;
-import com.cqut.czb.bn.entity.entity.MsgMap;
 import com.cqut.czb.bn.entity.entity.MsgModel;
 import com.cqut.czb.bn.entity.entity.User;
 import com.cqut.czb.bn.entity.global.JSONResult;
