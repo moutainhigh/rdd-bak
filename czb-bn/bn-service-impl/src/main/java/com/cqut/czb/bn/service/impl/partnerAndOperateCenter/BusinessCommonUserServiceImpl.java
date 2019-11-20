5ceee44e87c505cf89a374aeaed0d81f63893910
superstarQ <1903700887@qq.com>
2019-11-20 21:23:47 +0800

    美食服务修改

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/partnerAndOperateCenter/BusinessCommonUserServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/partnerAndOperateCenter/BusinessCommonUserServiceImpl.java
@@ -5,9 +5,11 @@ import com.cqut.czb.bn.dao.mapper.partnerAndOperateCenter.BusinessCommonUserVo;
 import com.cqut.czb.bn.entity.dto.PageDTO;
 import com.cqut.czb.bn.entity.dto.partnerAndOperateCenter.BusinessCommonUserOutputDTO;
 import com.cqut.czb.bn.service.partnerAndOperateCenter.BusinessCommonUserService;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -17,10 +19,10 @@ import java.util.List;
 public class BusinessCommonUserServiceImpl implements BusinessCommonUserService {
 
     @Autowired
-    BusinessCommonUserMapperExtra mapperExtra;
+    private BusinessCommonUserMapperExtra mapperExtra;
 
     @Override
-    public PageInfo<BusinessCommonUserOutputDTO> list(String userId, String mobile, Date createAt, Integer areaId, PageDTO pageDTO) {
+    public PageInfo<BusinessCommonUserOutputDTO> list(String userId, String mobile, @DateTimeFormat(pattern = "yyyy-MM-dd")Date createAt, Integer areaId, PageDTO pageDTO) {
         PageHelper.startPage(pageDTO.getCurrentPage(),pageDTO.getPageSize(),true);
         BusinessCommonUserVo vo = new BusinessCommonUserVo(userId,mobile,createAt,areaId);
         List<BusinessCommonUserOutputDTO> list = mapperExtra.list(vo);
