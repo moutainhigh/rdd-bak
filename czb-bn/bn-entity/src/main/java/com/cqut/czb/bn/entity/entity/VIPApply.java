af68e4255b56a6afaaa8ba487e587ac782fd89d6
name-beer <2789159051@qq.com>
2019-11-21 22:06:41 +0800

    申请会员数据获取(1)

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/VIPApply.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/entity/VIPApply.java
@@ -9,15 +9,7 @@ public class VIPApply extends PageDTO {
 
     private String vipName;
 
-    private Date createAt;
-
-    private Date updateAt;
-
-    private String recordId;
-
-    private String remark;
-
-    private Integer status;
+    private Integer audit;
 
     private String phoneNum;
 
@@ -33,24 +25,8 @@ public class VIPApply extends PageDTO {
         return vipName;
     }
 
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public String getRecordId() {
-        return recordId;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public Integer getStatus() {
-        return status;
+    public Integer getAudit() {
+        return audit;
     }
 
     public String getPhoneNum() {
@@ -69,24 +45,8 @@ public class VIPApply extends PageDTO {
         this.vipName = vipName;
     }
 
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public void setRecordId(String recordId) {
-        this.recordId = recordId;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
+    public void setAudit(Integer audit) {
+        this.audit = audit;
     }
 
     public void setPhoneNum(String phoneNum) {
