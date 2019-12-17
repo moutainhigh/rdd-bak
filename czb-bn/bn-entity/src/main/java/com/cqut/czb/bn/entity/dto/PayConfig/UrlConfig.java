bed0b1ad4fb760f0c742b96a39cd35c7ebc0f038
xuhaodong <1347929462@qq.com>
2019-12-17 20:46:23 +0800

    MASTER

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/PayConfig/UrlConfig.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/PayConfig/UrlConfig.java
@@ -6,9 +6,13 @@ package com.cqut.czb.bn.entity.dto.PayConfig;
 public class UrlConfig {
     public static final String NOTIGY_URL="47.110.9.136";
 
-    public static final String Port="8090";//端口号  106.91.24.192
+    public static final String Port="8090";//端口号
 //
 //    public static final String NOTIGY_URL="39.98.250.45";
 //
 //    public static final String Port="8899";//端口号
+
+    public static final String HTTPS="https";
+
+    public static final String HTTP="http";
 }
