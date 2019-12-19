2d729f44e312f5c5942814aea1ce2a2ee10f4af2
xuhaodong <1347929462@qq.com>
2019-12-19 16:32:55 +0800

    异常情况token丢失 微信小程序

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WCPUserInfoController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WCPUserInfoController.java
@@ -1,6 +1,7 @@
 package com.cqut.czb.bn.api.controller.WeChatSmallProgram;
 
 import com.cqut.czb.auth.util.RedisUtils;
+import com.cqut.czb.bn.entity.dto.WCPTabbarInfo;
 import com.cqut.czb.bn.entity.dto.user.UserDTO;
 import com.cqut.czb.bn.entity.global.JSONResult;
 import com.cqut.czb.bn.service.weChatSmallProgram.WCPUserInfoService;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
@@ -34,8 +37,26 @@ public class WCPUserInfoController {
 
     @GetMapping("/getTabBarInfo")
     public JSONResult getTabBarInfo(Principal principal){
-        UserDTO user = (UserDTO) redisUtils.get(principal.getName());
-        return new JSONResult(wcpUserInfoService.getTabBarInfo(user.getUserId()));
+        try {
+            UserDTO user = (UserDTO) redisUtils.get(principal.getName());
+            return new JSONResult(wcpUserInfoService.getTabBarInfo(user.getUserId()));
+        }catch (Exception e){
+            List<WCPTabbarInfo> tabbars = new ArrayList<>();
+            WCPTabbarInfo tabbarHomePage = new WCPTabbarInfo();
+            tabbarHomePage.setUrl("homePage");
+            tabbarHomePage.setImgNormal("/static/homePage.png");
+            tabbarHomePage.setImgClick("/static/homePageNo.png");
+            tabbarHomePage.setText("首页");
+
+            WCPTabbarInfo tabbarMine = new WCPTabbarInfo();
+            tabbarMine.setUrl("mine");
+            tabbarMine.setImgNormal("/static/mine_cilck.png");
+            tabbarMine.setImgClick("/static/mine.png");
+            tabbarMine.setText("我的");
+            tabbars.add(tabbarHomePage);
+            tabbars.add(tabbarMine);
+            return new JSONResult(tabbars);
+        }
     }
 
     @GetMapping("/getWCPCommodityCode")
