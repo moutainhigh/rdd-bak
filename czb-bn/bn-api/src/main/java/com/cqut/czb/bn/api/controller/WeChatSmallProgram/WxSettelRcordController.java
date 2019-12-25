0445e8f1e7b04d6fd0a169a3e11b26d8c96009df
name-beer <2789159051@qq.com>
2019-12-25 21:51:19 +0800

    结算记录

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WxSettelRcordController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WxSettelRcordController.java
@@ -1,4 +1,11 @@
 package com.cqut.czb.bn.api.controller.WeChatSmallProgram;
 
+import com.cqut.czb.bn.service.weChatSmallProgram.WxSettelRcordService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/WxSettelRcord")
 public class WxSettelRcordController {
+    WxSettelRcordService wxSettelRcordService;
 }
--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/WeChatSmallProgram/WxSettelRcordServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/WeChatSmallProgram/WxSettelRcordServiceImpl.java
@@ -1,4 +1,11 @@
 package com.cqut.czb.bn.service.impl.WeChatSmallProgram;
 
+import com.cqut.czb.bn.service.weChatSmallProgram.WxSettelRcordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WxSettelRcordServiceImpl {
+    @Autowired
+    WxSettelRcordService wxSettelRcordService;
 }
