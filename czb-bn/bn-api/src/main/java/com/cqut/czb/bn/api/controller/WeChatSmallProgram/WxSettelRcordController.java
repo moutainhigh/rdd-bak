bab372e1a8333bd827de9b2f790abe2745cd3e06
name-beer <2789159051@qq.com>
2019-12-27 09:28:34 +0800

    结算记录页面读取数据，删除，结算

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WxSettelRcordController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/WeChatSmallProgram/WxSettelRcordController.java
@@ -1,11 +1,28 @@
 package com.cqut.czb.bn.api.controller.WeChatSmallProgram;
 
+import com.cqut.czb.bn.entity.dto.WeChatSmallProgram.WeChatWithdrawDTO;
+import com.cqut.czb.bn.entity.dto.WeChatSmallProgram.WxSettleRcordDTO;
+import com.cqut.czb.bn.entity.global.JSONResult;
 import com.cqut.czb.bn.service.weChatSmallProgram.WxSettelRcordService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/WxSettelRcord")
 public class WxSettelRcordController {
     WxSettelRcordService wxSettelRcordService;
+
+    @GetMapping(value ="/selectSettleRcord")
+    public JSONResult selectSettleRcord(WxSettleRcordDTO pageDTO){
+        return wxSettelRcordService.getSettleRcord(pageDTO);
+}
+
+    @PostMapping("/settleRecord")
+    public JSONResult settleRecord(@RequestBody String recordId){
+        return  wxSettelRcordService.settleRecord(recordId);
+    }
+
+    @PostMapping("/deleteSettleRecord")
+    public JSONResult deleteSettleRecord(@RequestBody String recordId){
+        return  wxSettelRcordService.deleteSettleRecord(recordId);
+    }
 }
--- czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/weChatSmallProgram/WxSettelRcordMapperExtra.java
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/weChatSmallProgram/WxSettelRcordMapperExtra.java
@@ -1,4 +1,15 @@
 package com.cqut.czb.bn.dao.mapper.weChatSmallProgram;
 
+import com.cqut.czb.bn.entity.dto.WeChatSmallProgram.WxSettleRcordDTO;
+
+import java.util.List;
+
 public interface WxSettelRcordMapperExtra {
+    List<WxSettleRcordDTO> selectSettleRcord(WxSettleRcordDTO pageDTO);
+
+    boolean settleRecord(String recordId);
+
+    boolean deleteSettleRecord(String recordId);
+
+    boolean updateSettleRecord(String recordId);
 }
