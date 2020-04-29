ef71280a432fb924319e551df07a6b1006f1f733
name-beer <2789159051@qq.com>
2020-04-29 17:36:36 +0800

    中石化卡券导出

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/petrolCoupons/PetrolCouponsSalesRecordsServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/petrolCoupons/PetrolCouponsSalesRecordsServiceImpl.java
@@ -98,11 +98,12 @@ public class PetrolCouponsSalesRecordsServiceImpl implements PetrolCouponsSalesR
             }
         }
         int index = 0;
-        row = sheet.createRow(list.size()+1);
+        row = sheet.createRow(list.size());
         row.createCell(index++).setCellValue("销售总单数");
         row.createCell(index++).setCellValue("销售总金额");
         index = 0;
-        row = sheet.createRow(list.size());
+        row = sheet.createRow(list.size()+1);
+        row.createCell(index++).setCellValue(list.size()-1);
         row.createCell(index++).setCellValue(petrolCouponsSalesRecordsMapper.getPetrolCouponsSaleMoneyCount(inputDTO) + "元");
         return workbook;
     }
