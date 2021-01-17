a050c6e2f1d443bc65d25d99cf3575655da4d16a
name-beer <2789159051@qq.com>
2021-01-17 16:48:05 +0800

    H5订单查询

--- czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/directChargingSystem/OilCardRechargeMapperExtra.java
+++ czb-bn/bn-dao/src/main/java/com/cqut/czb/bn/dao/mapper/directChargingSystem/OilCardRechargeMapperExtra.java
@@ -1,4 +1,10 @@
 package com.cqut.czb.bn.dao.mapper.directChargingSystem;
 
+import com.cqut.czb.bn.entity.dto.directChargingSystem.DirectChargingOrderDto;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface OilCardRechargeMapperExtra {
+    List<DirectChargingOrderDto> getOrderInfoList(@Param("userId")String userId, @Param("type")Integer type);
 }
