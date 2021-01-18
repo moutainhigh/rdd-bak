67557d1177bf7489d8a5cec3112ab47ba05326af
hanxin00 <870120951@qq.com>
2021-01-18 16:15:42 +0800

    绑定油卡

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/directChargingSystem/OilCardRechargeServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/directChargingSystem/OilCardRechargeServiceImpl.java
@@ -25,12 +25,12 @@ public class OilCardRechargeServiceImpl implements OilCardRechargeService {
 
     @Override
     public JSONResult bindingOilCard(String userId, OilCardBinging oilCardBinging) {
-        if(oilCardBinging.getPertrolNum().equals(oilCardBinging.getIsPertrolNum()))
-            return new JSONResult("输入卡号不一致");
+        if(!oilCardBinging.getPertrolNum().equals(oilCardBinging.getIsPertrolNum()))
+            return new JSONResult(500,"输入卡号不一致");
 
         int oilCardNum = oilCardRechargeMapperExtra.isExistOilCard(oilCardBinging);
         if (oilCardNum > 0)
-            return new JSONResult("该卡号已被绑定过,请重新确认卡号");
+            return new JSONResult(500,"该卡号已被绑定过,请重新确认卡号");
 
         // 检查该用户有没有绑过卡号
         int isExist = oilCardRechargeMapperExtra.isExistOilCardUser(userId);
@@ -47,6 +47,6 @@ public class OilCardRechargeServiceImpl implements OilCardRechargeService {
                 userCardRelation.setSinopecPetrolNum(oilCardBinging.getPertrolNum());
             isSuccess = oilCardRechargeMapperExtra.insertPetrolNum(userCardRelation) > 0;
         }
-        return isSuccess ? new JSONResult("绑定成功"):new JSONResult("绑定失败");
+        return isSuccess ? new JSONResult(200,"绑定成功"):new JSONResult(500,"绑定失败");
     }
 }
