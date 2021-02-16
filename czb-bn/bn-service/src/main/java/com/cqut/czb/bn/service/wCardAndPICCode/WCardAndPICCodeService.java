b090342f1d501a16ac524d48cae5f9af0bd17a32
hd553515 <68462569+hd553515@users.noreply.github.com>
2021-02-16 21:16:30 +0800

    我的订单

--- czb-bn/bn-service/src/main/java/com/cqut/czb/bn/service/wCardAndPICCode/WCardAndPICCodeService.java
+++ czb-bn/bn-service/src/main/java/com/cqut/czb/bn/service/wCardAndPICCode/WCardAndPICCodeService.java
@@ -1,7 +1,6 @@
 package com.cqut.czb.bn.service.wCardAndPICCode;
 
 import com.cqut.czb.bn.entity.dto.wCardAndPICCode.WCardAndPICCodeDTO;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
