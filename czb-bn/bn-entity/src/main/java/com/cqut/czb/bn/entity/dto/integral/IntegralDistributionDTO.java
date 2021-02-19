c653f9b5c04f9e595959c94156fd3d0b555817c4
houjialing <2280852020@qq.com>
2021-02-19 23:11:05 +0800

    时间格式转化修改

--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/integral/IntegralDistributionDTO.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/integral/IntegralDistributionDTO.java
@@ -15,7 +15,7 @@ public class IntegralDistributionDTO extends PageDTO {
     private Integer type;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss", timezone = "GMT+8")
     private Date createAt;
 
     private String IntegralExchange;
@@ -25,7 +25,7 @@ public class IntegralDistributionDTO extends PageDTO {
     private Integer isComplete;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss", timezone = "GMT+8")
     private Date failureTime;
 
     public Integer getIntegralAmount() {
--- czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/integral/IntegralExchangeDTO.java
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/integral/IntegralExchangeDTO.java
@@ -23,7 +23,7 @@ public class IntegralExchangeDTO {
     private Integer isComplete;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss", timezone = "GMT+8")
     private Date failureTime;
 
     private Date createAt;
