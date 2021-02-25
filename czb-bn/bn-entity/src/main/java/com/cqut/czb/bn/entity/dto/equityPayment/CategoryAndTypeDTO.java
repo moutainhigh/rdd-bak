1dcba3c7fa365dd718c26899b7fe4dcdffad906e
yuansonghe <2722746759@qq.com>
2021-02-25 19:49:20 +0800

    权益商品管理

---
+++ czb-bn/bn-entity/src/main/java/com/cqut/czb/bn/entity/dto/equityPayment/CategoryAndTypeDTO.java
@@ -0,0 +1,44 @@
+package com.cqut.czb.bn.entity.dto.equityPayment;
+
+public class CategoryAndTypeDTO {
+
+    private String categoryId;
+
+    private String categoryName;
+
+    private String typeId;
+
+    private String typeName;
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(String typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+}
