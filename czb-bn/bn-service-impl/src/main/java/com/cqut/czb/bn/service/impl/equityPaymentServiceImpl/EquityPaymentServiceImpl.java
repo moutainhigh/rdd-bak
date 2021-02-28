3e1ee52a1dfa0968217d713bce161786e0109471
houjialing <2280852020@qq.com>
2021-02-28 19:41:17 +0800

    删除权益商品时，同时删除图片

--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/equityPaymentServiceImpl/EquityPaymentServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/equityPaymentServiceImpl/EquityPaymentServiceImpl.java
@@ -64,9 +64,16 @@ public class EquityPaymentServiceImpl implements EquityPaymentService {
     }
 
     public JSONResult deleteCommodity(String goodsId) {
-        int column = equityPaymentCommodityMapperExtra.deleteCommodity(goodsId, true);
-        if (column == 0) {
+        EquityPaymentCommodityDTO equityPaymentCommodityDTO = equityPaymentCommodityMapperExtra.selectCommodityByGoodsId(goodsId);
+        int column1 = equityPaymentCommodityMapperExtra.deleteCommodity(goodsId, true);
+        if (column1 == 0) {
             return new JSONResult("删除商品失败");
+        }
+        if (equityPaymentCommodityDTO.getGoodsPic() != null || !equityPaymentCommodityDTO.getGoodsPic().equals("")) {
+            int column2 = fileMapperExtra.deleteByPrimaryKey(equityPaymentCommodityDTO.getGoodsPic());
+            if (column2 == 0) {
+                return new JSONResult("删除商品失败");
+            }
         }
 
         return new JSONResult("删除商品成功");
