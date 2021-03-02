336204fc5f0cb533eb6c1626cdf2cdf820f50f28
yuansonghe <2722746759@qq.com>
2021-03-02 09:16:57 +0800

    区服管理

--- czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/equityPayment/AreaClothingController.java
+++ czb-bn/bn-api/src/main/java/com/cqut/czb/bn/api/controller/equityPayment/AreaClothingController.java
@@ -4,6 +4,7 @@ import com.cqut.czb.bn.entity.dto.PageDTO;
 import com.cqut.czb.bn.entity.dto.equityPayment.EquityPaymentAreaClothingDTO;
 import com.cqut.czb.bn.entity.global.JSONResult;
 import com.cqut.czb.bn.service.equityPaymentService.AreaClothingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/equityPaymentAreaClothing")
 public class AreaClothingController {
 
+    @Autowired
     AreaClothingService areaClothingService;
 
     /**
--- czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/equityPaymentServiceImpl/AreaClothingServiceImpl.java
+++ czb-bn/bn-service-impl/src/main/java/com/cqut/czb/bn/service/impl/equityPaymentServiceImpl/AreaClothingServiceImpl.java
@@ -9,9 +9,11 @@ import com.cqut.czb.bn.util.string.StringUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AreaClothingServiceImpl implements AreaClothingService {
 
     @Autowired
